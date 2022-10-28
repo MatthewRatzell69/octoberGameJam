d25582e3fb775c3bb97fb5cf4cf87342907e4392
Matthew Ratzell <54945701+MatthewRatzell@users.noreply.github.com>
2022-10-28 19:59:35 -0400

    Smoney Smoves

---
+++ Assets/Scripts/OpenSpot.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSpot : MonoBehaviour
+{
+    //these variables are for all the different towers it could be 
+    public GameObject skeleton;
+    public GameObject thorns;
+
+    private GameObject towerPrefabToPlace;
+    private GameObject tower;
+
+    private bool CanPlaceTower()
+    {
+        //if the openspot doesnt have a monster return true
+        if (tower == null)
+        {
+            return true;
+        }
+
+        return false;
+        
+    }
+
+    //function built into unity that fires when something is clicked and dragged
+    void OnMouseUp()
+    {
+        
+        if (CanPlaceTower())
+        {
+            //get the currently selected monster prefab from the game manager
+            switch (gameManager.monsterSelected)
+            {
+                case "SkeletonShop":
+                    Debug.Log("Placing Skeleton");
+                    towerPrefabToPlace = skeleton;
+                    break;
+                case "ThornsShop":
+                    Debug.Log("Placing Thorns");
+                    towerPrefabToPlace = thorns;
+                    break;
+                    //need to add all of the other cases for other towers
+                default:
+                    
+                    break;
+            }
+
+            //turn off the open spaces sprite
+            gameObject.GetComponent<SpriteRenderer>().sprite=null;
+            //if monster can be placed, place it on this spot 
+            tower = Instantiate(towerPrefabToPlace, transform.position, Quaternion.identity);
+ 
+
+            
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/gameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+
+    //the currently selected monster in the shop, right now being preset to the skeleton
+    public static string monsterSelected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
